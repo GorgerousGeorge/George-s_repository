edb41faeb7c66781402c3ef549a4146ac514c3a0
Georgiy Sinitsyn <gorgerousgeorge@gmail.com>
2024-11-03 09:28:44 +0300

    оформление кода по требованиям тз

--- src/widget.py
+++ src/widget.py
@@ -3,19 +3,22 @@ import masks
 
 def mask_account_card(card_info: str) -> str:
     """Принимает тип и номер карты/счета, возвращает замаскированный номер карты/счета"""
-    card_info = card_info.split()
-    print(card_info)
-    if card_info[0] == "Счет":
-        returned_number = masks.get_mask_account(card_info.pop(-1))
+    card_infolist = card_info.split()
+    print(card_infolist)
+    if card_infolist[0] == "Счет":
+        returned_number = masks.get_mask_account(card_infolist.pop(-1))
     else:
-        returned_number = masks.get_mask_card_number(card_info.pop(-1))
-    card_info.append(returned_number)
-    card_info = " ".join(card_info)
-    return card_info
+        returned_number = masks.get_mask_card_number(card_infolist.pop(-1))
+    card_infolist.append(returned_number)
+    card_infolist = " ".join(card_infolist)
+    return str(card_infolist)
 
 
 def get_date(core_date: str) -> str:
     """Принимает дату и время в формате ISO 8601, возвращает дату в формате ДД.ММ.ГГГГ"""
-    core_date = core_date.split("-")
-    returned_date = core_date[2][:2] + "." + core_date[1] + "." + core_date[0]
+    core_date_list = core_date.split("-")
+    returned_date = core_date_list[2][:2] + "." + core_date_list[1] + "." + core_date_list[0]
     return returned_date
+
+
+print(mask_account_card("Maestro 1596837868705199"))
