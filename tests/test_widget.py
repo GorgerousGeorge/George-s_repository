6789d79401f92d1d31d959278579125735da3add
Georgiy Sinitsyn <gorgerousgeorge@gmail.com>
2024-11-15 20:44:12 +0300

    исправлены ошибки в наименовании тестовых функций

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,8 +1,8 @@
 from src.widget import mask_account_card
 from src.widget import get_date
 
-def test mask_account_card():
+def test_mask_account_card():
     assert mask_account_card("Visa 1234567890123456") == "1234 56** **** 3456"
 
-def test get_date():
+def test_get_date():
     assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
