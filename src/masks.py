203c7511d00708c9de356c61428d6a1e6a5c01b7
Georgiy Sinitsyn <gorgerousgeorge@gmail.com>
2024-11-17 15:11:56 +0300

    исправлена ошибка при которой функции из модуля masks.py не маскировали номера карт и счетов, переданных с пробелами

--- src/masks.py
+++ src/masks.py
@@ -4,7 +4,7 @@ from typing import Any
 def get_mask_card_number(card_number: Any) -> str:
     """Принимает на вход номер карты, возвращает маску карты в типе str"""
     card_number = str(card_number)
-    card_number.replace(" ", "")
+    card_number = card_number.replace(" ", "")
     if card_number.isdigit() and 12 < len(card_number) < 20:
         masked_card_number = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
         return str(masked_card_number)
@@ -14,7 +14,7 @@ def get_mask_card_number(card_number: Any) -> str:
 def get_mask_account(account_number: Any) -> str:
     """Принимет номер счета, возвращает маску счета в типе str"""
     account_number = str(account_number)
-    account_number.replace(" ", "")
+    account_number = account_number.replace(" ", "")
     if account_number.isdigit() and len(account_number) == 20:
         masked_account = "**" + account_number[-4:]
         return str(masked_account)
