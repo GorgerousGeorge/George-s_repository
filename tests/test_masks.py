f4d2600e31a76381af53f12f67235d8b45c63e21
Georgiy Sinitsyn <gorgerousgeorge@gmail.com>
2024-11-17 13:52:06 +0300

    добавлены тесты для модуля masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -4,111 +4,57 @@ from src.masks import get_mask_card_number
 from src.masks import get_mask_account
 
 
-@pytest.fixture
-def numbers_16():
-    return "7000792289606361"
+def test_get_mask_card_number(numbers_16):
+    assert get_mask_card_number(numbers_16) == "7000 79** **** 6361"
 
 
-@pytest.fixture
-def numbers_16_spases():
-    return "7000 7922 8960 6361"
+def test_get_mask_card_number(numbers_16_spases):
+    assert get_mask_card_number(numbers_16_spases) == "7000 79** **** 6361"
 
 
-@pytest.fixture
-def numbers_16_int():
-    return 7000792289606361
+def test_get_mask_card_number(numbers_16_int):
+    assert get_mask_card_number(numbers_16_int) == "7000 79** **** 6361"
 
 
-@pytest.fixture
-def numbers_20():
-    return "70007922896063611234"
+def test_get_mask_card_number(numbers_19):
+    assert get_mask_card_number(numbers_19) == "7000 79** **** 1123"
 
 
-@pytest.fixture
-def numbers_20_spases():
-    return "70007 92289 60636 11234"
-
-
-@pytest.fixture
-def numbers_20_int():
-    return 70007922896063611234
-
-
-@pytest.fixture
-def numbers_19():
-    return "7000792289606361123"
-
-
-@pytest.fixture
-def numbers_21():
-    return "700079228960636112345"
-
-
-@pytest.fixture
-def numbers_13():
-    return "7000792289606"
-
-
-@pytest.fixture
-def numbers_12():
-    return "700079228960"
-
-
-@pytest.fixture
-def letters():
-    return "тестовые_буквы"
-
-
-@pytest.fixture
-def numbers_and_letters():
-    return "a1b2c3d4e5f6g7h8i9j0"
-
-
-@pytest.fixture
-def blank():
-    return ""
+def test_get_mask_card_number(numbers_13):
+    assert get_mask_card_number(numbers_13) == "7000 79** **** 9606"
 
 
 @pytest.mark.parametrize("value, expected", [
-    ("7000792289606361", "7000 79** **** 6361"),
-    (numbers_16, "7000 79** **** 6361"),
-    (numbers_16_spases, "7000 79** **** 6361"),
-    (numbers_16_int, "7000 79** **** 6361"),
-    (numbers_19, "7000 79** **** 1123"),
-    (numbers_13, "7000 79** **** 9606")
-])
-def test_get_mask_card_number(value, expected):
-    assert get_mask_card_number(value) == expected
-
-
-@pytest.mark.parametrize("value, expected", [
-    (numbers_20, "некорректный номер карты"),
-    (numbers_12, "некорректный номер карты"),
-    (letters, "некорректный номер карты"),
-    (numbers_and_letters, "некорректный номер карты"),
-    (blank, "некорректный номер карты")
+    ("70007922896063611234", "некорректный номер карты"),
+    ("700079228960", "некорректный номер карты"),
+    ("тестовые_буквы", "некорректный номер карты"),
+    ("a1b2c3d4e5f6g7h8i9j0", "некорректный номер карты"),
+    ("", "некорректный номер карты")
 ])
 def test_get_mask_card_number_valueerror(value, expected):
     with pytest.raises(ValueError):
         get_mask_card_number(value) == expected
 
 
-@pytest.mark.parametrize("value, expected", [
-    (numbers_20, "**1234"),
-    (numbers_20_spases, "**1234"),
-    (numbers_20_int, "**1234"),
-])
-def test_get_mask_account(value, expected):
-    assert get_mask_account(value) == expected
+def test_get_mask_account(numbers_20):
+    assert get_mask_account(numbers_20) == "**1234"
+
+
+def test_get_mask_account(numbers_20_spases):
+    assert get_mask_account(numbers_20_spases) == "**1234"
+
+
+def test_get_mask_account(numbers_20_int):
+    assert get_mask_account(numbers_20_int) == "**1234"
 
 
 @pytest.mark.parametrize("value, expected", [
-    (numbers_16, "некорректный номер счета"),
-    (numbers_19, "некорректный номер счета"),
-    (letters, "некорректный номер счета"),
-    (numbers_and_letters, "некорректный номер счета"),
-    (blank, "некорректный номер счета"),
-    (numbers_21, "некорректный номер счета"),
+    ("7000792289606361", "некорректный номер счета"),
+    ("7000792289606361123", "некорректный номер счета"),
+    ("тестовые_буквы", "некорректный номер счета"),
+    ("a1b2c3d4e5f6g7h8i9j0", "некорректный номер счета"),
+    ("", "некорректный номер счета"),
+    ("700079228960636112345", "некорректный номер счета"),
 ])
 def test_get_mask_account_valueerror(value, expected):
     with pytest.raises(ValueError):
