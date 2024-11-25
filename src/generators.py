14eeaa39df46ae19eb9b37a023af73d06a1bdefb
Georgiy Sinitsyn <gorgerousgeorge@gmail.com>
2024-11-25 20:07:08 +0300

    добавлена функция transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -1,6 +1,25 @@
-def filter_by_currency(transaction_list:list[dict], valute: str = "USD") -> dict:
+def filter_by_currency(transaction_list: list[dict], valute: str = "USD") -> dict:
+    """Принимает список словарей, представляющих транзакции. Возвращает по одному словарю из списка, в котором валюта
+    операции соответствует заданной (по умолчанию USD, для изменения вторым аргументом надо передать буквенный код
+    валюты)"""
     for transaction in transaction_list:
         if transaction["operationAmount"]["currency"]["code"] == valute:
             yield transaction
 
 
+def transaction_descriptions(transaction_list: list[dict]) -> str:
+    """Принимает список словарей, представляющих транзакции. Возвращает описания каждой операции из списка по одному"""
+    for transaction in transaction_list:
+        yield transaction["description"]
+
+
+def card_number_generator(start: int, finish: int) -> str:
+    """Генерирует 16-значные номера карт в рамках заданнного диапазона. При вызове генератора первым аргументом передаем
+    начальное значение генерации диапазона, вторым аргументом передаем конечное значение диапазона (включительно)"""
+    blank_number = "0000000000000000"
+    for card_number in range(start, finish + 1):
+        card_number = str(card_number)
+        return_number = blank_number[:-len(card_number)] + card_number
+        return_number = (return_number[:4] + " " + return_number[4:8] + " " + return_number[8:12] + " " +
+                         return_number[12:])
+        yield return_number
