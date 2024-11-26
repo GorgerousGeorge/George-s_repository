4d07380bc2ef8399057806e0e8aa030111b31052
Georgiy Sinitsyn <gorgerousgeorge@gmail.com>
2024-11-26 17:24:19 +0300

    правки тестовых кейсов

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,25 +2,15 @@ import pytest
 
 from src.masks import get_mask_account, get_mask_card_number
 
-
-def test_get_mask_card_number(numbers_16):
-    assert get_mask_card_number(numbers_16) == "7000 79** **** 6361"
-
-
-def test_get_mask_card_number_with_spaces(numbers_16_spases):
-    assert get_mask_card_number(numbers_16_spases) == "7000 79** **** 6361"
-
-
-def test_get_mask_card_number_int(numbers_16_int):
-    assert get_mask_card_number(numbers_16_int) == "7000 79** **** 6361"
-
-
-def test_get_mask_card_number_maxnumber(numbers_19):
-    assert get_mask_card_number(numbers_19) == "7000 79** **** 1123"
-
-
-def test_get_mask_card_number_minnumber(numbers_13):
-    assert get_mask_card_number(numbers_13) == "7000 79** **** 9606"
+@pytest.mark.parametrize("value, expected", [
+    ("7000792289606361", "7000 79** **** 6361"),
+    ("7000 7922 8960 6361", "7000 79** **** 6361"),
+    (7000792289606361, "7000 79** **** 6361"),
+    ("7000792289606361123", "7000 79** **** 1123"),
+    ("7000792289606", "7000 79** **** 9606")
+])
+def test_get_mask_card_number(value, expected):
+    assert get_mask_card_number(value) == expected
 
 
 @pytest.mark.parametrize("value, expected", [
@@ -40,16 +30,8 @@ def test_get_mask_card_number_valueerror(value, expected):
     ("70007 92289 60636 11234", "**1234"),
     (70007922896063611234, "**1234"),
 ])
-def test_get_mask_account(numbers_20):
-    assert get_mask_account(numbers_20) == "**1234"
-
-
-def test_get_mask_account_with_spaces(numbers_20_spases):
-    assert get_mask_account(numbers_20_spases) == "**1234"
-
-
-def test_get_mask_account_int(numbers_20_int):
-    assert get_mask_account(numbers_20_int) == "**1234"
+def test_get_mask_account(value, expected):
+    assert get_mask_account(value) == expected
 
 
 @pytest.mark.parametrize("value, expected", [
