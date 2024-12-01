4f627ec7b5adf028fefe6d75075623868d719be7
Georgiy Sinitsyn <gorgerousgeorge@gmail.com>
2024-12-01 17:48:31 +0300

    доавлены тесты для проверки работы декоратора log

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,5 +1,6 @@
-import pytest
 import os
+
+import pytest
 
 from src.decorators import log
 
@@ -15,12 +16,12 @@ def working_function_with_file():
 
 
 @log()
-def error_function_console():
+def error_function_console(*args, **kwargs):
     raise ValueError("Something went wrong!")
 
 
 @log(filename="testdecor.txt")
-def error_function_with_file():
+def error_function_with_file(*args, **kwargs):
     raise ValueError("Something went wrong!")
 
 
@@ -37,8 +38,30 @@ def test_log_working_decorator_file():
         assert lines[-1] == "working_function_with_file ok \n"
 
 
-def test_log_error_console(capsys):
+@pytest.mark.parametrize("positionarg, namearg, expected", [
+    ((), {}, "error_function_console error: Something went wrong!. Inputs: (), {}\n"),
+    (("a", "b"), {}, "error_function_console error: Something went wrong!. Inputs: ('a', 'b'), {}\n"),
+    ((), {'argument': '1'}, "error_function_console error: Something went wrong!. Inputs: (), {'argument':'1'}\n"),
+    (("a", "b"), {'argument': '1'},
+     "error_function_console error: Something went wrong!. Inputs: ('a', 'b'), {'argument':'1'}\n")
+])
+def test_log_error_console(capsys, positionarg, namearg, expected):
     with pytest.raises(ValueError):
-        error_function_console()
+        error_function_console(*positionarg, **namearg)
         captured = capsys.readouterr()
-        assert captured.out == "error_function_console error: Something went wrong!. Inputs: (), {}\n"
+        assert captured.out == expected
+
+
+@pytest.mark.parametrize("positionarg, namearg, expected", [
+    ((), {}, "error_function_console error: Something went wrong!. Inputs: (), {}\n"),
+    (("a", "b"), {}, "error_function_console error: Something went wrong!. Inputs: ('a', 'b'), {}\n"),
+    ((), {'argument': '1'}, "error_function_console error: Something went wrong!. Inputs: (), {'argument':'1'}\n"),
+    (("a", "b"), {'argument': '1'},
+     "error_function_console error: Something went wrong!. Inputs: ('a', 'b'), {'argument':'1'}\n")
+])
+def test_log_error_file(positionarg, namearg, expected):
+    with pytest.raises(ValueError):
+        error_function_with_file(*positionarg, **namearg)
+        with open(f"{os.getcwd()}\\logs\\testdecor.txt", "r") as file:
+            lines = file.readlines()
+            assert lines[-1] == expected
