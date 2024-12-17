7153e51659bb42fa20118d96a86308474c11071d
Georgiy Sinitsyn <gorgerousgeorge@gmail.com>
2024-12-17 22:51:35 +0300

    правки main

--- src/main.py
+++ src/main.py
@@ -45,39 +45,84 @@ def main():
             print(f"Статус операции {status_for_filter} недоступен")
     print(transaction_list)
     transaction_list = filter_by_state(transaction_list, status_for_filter)
-    print("Отсортировать операции по дате? Да/Нет")
-    answer_for_date = input()
-    if answer_for_date.lower() == "да" or answer_for_date.lower() == "yes":
-        print("Отсортировать по возрастанию или по убыванию?")
-        answer_for_order = input()
-        order_for_date = (answer_for_order.lower == "по убыванию")
-        transaction_list = sort_by_date(transaction_list, order_for_date)
-    print("Выводить только рублевые тразакции? Да/Нет")
-    answer_for_currency = input()
-    if answer_for_currency.lower() == "да" or answer_for_currency.lower() == "yes":
-        support_list = []
-        for transaction in transaction_list:
-            if transaction["operationAmount"]["currency"]["code"] == "RUB":
-                support_list.append(transaction)
-        transaction_list = support_list
-    print("Отфильтровать список транзакций по определенному слову в описании? Да/Нет")
-    answer_for_description = input()
-    if answer_for_description.lower() == "да" or answer_for_description.lower() == "yes":
-        print("Введите слово, по которому будем фильтровать список")
-        user_description = input()
-        transaction_list = filter_by_description(transaction_list, user_description)
-    print(f"Распечатываю итоговый список транзакций...\nВсего банковских операций в выборке: {len(transaction_list)}")
-    for transaction in transaction_list:
-        if transaction["description"] == "Открытие вклада":
-            print(f"{get_date(transaction["date"])} {transaction["description"]}\n"
-                  f"{mask_account_card(transaction["to"])}\nСумма: {transaction["operationAmount"]["amount"]} "
-                  f"{transaction["operationAmount"]["currency"]["name"]}")
+    while True:
+        print("Отсортировать операции по дате? Да/Нет")
+        answer_for_date = input()
+        if answer_for_date.lower() == "да" or answer_for_date.lower() == "yes":
+            while True:
+                print("Отсортировать по возрастанию или по убыванию?")
+                answer_for_order = input()
+                if "возрастан" in answer_for_order.lower() or "убыван" in answer_for_order.lower():
+                    order_for_date = (answer_for_order.lower == "по убыванию")
+                    transaction_list = sort_by_date(transaction_list, order_for_date)
+                    break
+                else:
+                    print("Выбор не распознан")
+                    continue
+            break
+        elif answer_for_date.lower() == "нет" or answer_for_date.lower() == "no":
+            break
         else:
-            print(f"{get_date(transaction["date"])} {transaction["description"]}\n"
-                  f"{mask_account_card(transaction["from"])} -> {mask_account_card(transaction["to"])}\n"
-                  f"Сумма: {transaction["operationAmount"]["amount"]} "
-                  f"{transaction["operationAmount"]["currency"]["name"]}")
-    if transaction_list == False:
+            print("Ответ не распознан")
+    while True:
+        print("Выводить только рублевые тразакции? Да/Нет")
+        answer_for_currency = input()
+        if answer_for_currency.lower() == "да" or answer_for_currency.lower() == "yes":
+            support_list = []
+            if descr_type_file == "JSON":
+                for transaction in transaction_list:
+                    if transaction["operationAmount"]["currency"]["code"] == "RUB":
+                        support_list.append(transaction)
+            else:
+                for transaction in transaction_list:
+                    if transaction["currency_code"] == "RUB":
+                        support_list.append(transaction)
+            transaction_list = support_list
+            break
+        elif answer_for_currency.lower() == "нет" or answer_for_currency.lower() == "no":
+            break
+        else:
+            print("Ответ не распознан")
+    while True:
+        print("Отфильтровать список транзакций по определенному слову в описании? Да/Нет")
+        answer_for_description = input()
+        if answer_for_description.lower() == "да" or answer_for_description.lower() == "yes":
+            print("Введите слово, по которому будем фильтровать список")
+            user_description = input()
+            transaction_list = filter_by_description(transaction_list, user_description)
+            break
+        elif nswer_for_description.lower() == "нет" or nswer_for_description.lower() == "no":
+            break
+        else:
+            print("Ответ не распознан")
+    print(f"Распечатываю итоговый список транзакций...\n")
+    if len(transaction_list) == 0:
         print("Не найдено ни одной транзакции, подходящей под ваши условия фильтрации")
+    else:
+        print(f"Всего банковских операций в выборке: {len(transaction_list)}")
+        if descr_type_file == "JSON":
+            for transaction in transaction_list:
+                if transaction["description"] == "Открытие вклада":
+                    print(f"{get_date(transaction["date"])} {transaction["description"]}\n"
+                          f"{mask_account_card(transaction["to"])}\nСумма: {transaction["operationAmount"]["amount"]} "
+                          f"{transaction["operationAmount"]["currency"]["name"]}")
+                else:
+                    print(f"{get_date(transaction["date"])} {transaction["description"]}\n"
+                          f"{mask_account_card(transaction["from"])} -> {mask_account_card(transaction["to"])}\n"
+                          f"Сумма: {transaction["operationAmount"]["amount"]} "
+                          f"{transaction["operationAmount"]["currency"]["name"]}")
+        else:
+            for transaction in transaction_list:
+                if transaction["currency_name"] == 'Ruble':
+                    transaction["currency_name"] = "руб."
+                if transaction["description"] == "Открытие вклада":
+                    print(f"{get_date(transaction["date"])} {transaction["description"]}\n"
+                          f"{mask_account_card(transaction["to"])}\nСумма: {transaction["amount"]} "
+                          f"{transaction["currency_name"]}")
+                else:
+                    print(f"{get_date(transaction["date"])} {transaction["description"]}\n"
+                          f"{mask_account_card(transaction["from"])} -> {mask_account_card(transaction["to"])}\n"
+                          f"Сумма: {transaction["amount"]} {transaction["currency_name"]}")
+
 
 main()
